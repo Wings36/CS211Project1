117370c834413cdf6c6876ef145d6a67b3a99968
Peter Hoang <peterhg7@gmail.com>
2020-01-22 13:53:12 -0800

    Implemented transfering inheritance data from file to database. Not Complete

--- src/MainUser.java
+++ src/MainUser.java
@@ -1,11 +1,47 @@
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class MainUser {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
 
+        final String keyword = "END";
         File personFile = new File("tudor.dat");
 
+        //Creating person objects
+
+
+
+
+
+        //Finding Inheritance of the person
+
+        Scanner file = new Scanner(personFile);
+
+        while(file.hasNext()) {
+            String personRead = file.nextLine();
+            int index;
+            //End file detector
+            if (personRead.equals(keyword))
+                break;
+            //implement arraylist
+            //scan arraylist to find the person
+            for (int x :) {
+                if (personRead.equals()) {
+                    index = x;
+                    break;
+                }
+            }
+
+            //Add in mother
+            personRead = file.nextLine();
+
+            //Add in father
+            personRead = file.nextLine();
+
+
+        }
 
     }
 }
