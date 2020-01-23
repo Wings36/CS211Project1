2093b39f89d0caddf61ae103e8fb60fad9f5f075
BuildTools <unconfigured@null.spigotmc.org>
2020-01-22 23:49:02 -0800

    Implemented reading from the file to create the Person objects

--- src/MainUser.java
+++ src/MainUser.java
@@ -1,6 +1,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class MainUser {
@@ -9,31 +10,41 @@ public class MainUser {
 
         final String keyword = "END";
         File personFile = new File("tudor.dat");
+        Scanner input = new Scanner(new File("tudor.dat"));
 
         //Creating person objects
+        List<Person> people = new ArrayList<>();
 
+        // Read each line
+        while (input.hasNextLine()) {
+            String line = input.nextLine();
+            Person p = new Person(line);
+            people.add(p);
+        }
 
+        for (Person p: people) {
+            System.out.println(p.getName());
+        }
 
 
 
         //Finding Inheritance of the person
 
-        Scanner file = new Scanner(personFile);
+
         ArrayList<Person> temp = new ArrayList<>();
 
-        while(file.hasNext()) {
-            String personRead = file.nextLine();
+        while(input.hasNext()) {
+            String personRead = input.nextLine();
             if (personRead.equals(keyword))
                 break;
 
             //replace temp with the People Array
             //scan arraylist to find the person.
-            for (Person personMain : temp) {
+            for (Person personMain : people) {
                 if (personMain.getName().equals(personRead)) {
-
                     for (int x = 0; x < 2 ; x++) {
-                        personRead = file.nextLine();
-                        for (Person parent : temp) {
+                        personRead = input.nextLine();
+                        for (Person parent : people) {
                             if (parent.getName().equals(personRead)) {
                                 if (x == 0)
                                     personMain.setMother(parent);
@@ -45,6 +56,7 @@ public class MainUser {
                 }
             }
         }
+        System.out.println(people.get(1).getFather());
     }
 }
 
