7a2f397482a0bfd3a5391f76edf5867e21a931ac
BuildTools <unconfigured@null.spigotmc.org>
2020-01-22 18:32:00 -0800

    Finished Person Class

--- src/Person.java
+++ src/Person.java
@@ -1,2 +1,40 @@
+import java.util.*;
+
 public class Person {
+    private String name;
+    private Person father;
+    private Person mother;
+    private ArrayList<Person> children;
+
+    public Person(String name) {
+        this.name=name;
+        children=new ArrayList<Person>();
+    }
+    //Set data
+    public void setName(String name) {
+        this.name=name;
+    }
+    public void setFather(Person father) {
+        this.father=father;
+    }
+    public void setMother(Person mother) {
+        this.mother=mother;
+    }
+    public void addChildren(Person child) {
+        children.add(child);
+    }
+
+    //Pull data
+    public String getName() {
+        return name;
+    }
+    public Person getFather() {
+        return father;
+    }
+    public Person getMother() {
+        return mother;
+    }
+    public ArrayList<Person> getChildren() {
+        return children;
+    }
 }
