34ddbad904f42e7ad078f6adad6d69251ee163c2
honchen2000 <honchen2000@gmail.com>
2020-01-26 15:29:39 -0800

    I made the user interface.

--- src/MainUser.java
+++ src/MainUser.java
@@ -26,10 +26,9 @@ public class MainUser {
             people.add(p);
         }
 
-        for (Person p: people) {
+        for (Person p : people) {
             System.out.println(p.getName());
         }
-
 
 
         //Finding Inheritance of the person
@@ -37,7 +36,7 @@ public class MainUser {
 
         ArrayList<Person> temp = new ArrayList<>();
 
-        while(input.hasNext()) {
+        while (input.hasNext()) {
             String personRead = input.nextLine();
             if (personRead.equals(keyword))
                 break;
@@ -46,7 +45,7 @@ public class MainUser {
             //scan arraylist to find the person.
             for (Person personMain : people) {
                 if (personMain.getName().equals(personRead)) {
-                    for (int x = 0; x < 2 ; x++) {
+                    for (int x = 0; x < 2; x++) {
                         personRead = input.nextLine();
                         for (Person parent : people) {
                             if (parent.getName().equals(personRead)) {
@@ -61,7 +60,47 @@ public class MainUser {
                 }
             }
         }
+        output(people);
         //System.out.println(people.get(1).getFather());
+    }
+    //User interface
+    public static void output(List<Person> list) {
+        Person p=inputdata(list);
+        if(p==null){
+            System.out.println("I cannot find this name in the list.");
+        }
+        else{
+            System.out.println("Maternal line: ");
+            System.out.println("    "+p.getName());
+            System.out.println("        "+p.getMother().getName());
+            System.out.println("Paternal line: ");
+            System.out.println("    "+p.getName());
+            System.out.println("        "+p.getFather().getName());
+            System.out.println("Children: ");
+            for(int i=p.getChildren().size()-1;i>=0;i--){
+                String child=p.getChildren().get(i).getName();
+                System.out.println(child);
+            }
+        }
+
+    }
+    public static Person inputdata(List<Person> list){
+        Scanner console = new Scanner(System.in);
+        System.out.println("Person's name?");
+        String input = console.next();
+        Person p=searchList1(input, list);
+        return p;
+    }
+    public static Person searchList1(String person, List<Person> list) {
+        String result = "";
+        Person p = null;
+        for(int i = 0; i < list.size(); i++) {
+            result = list.get(i).getName();
+            if(result.contains(person)) {
+                p=list.get(i);
+            }
+        }
+        return p;
     }
     //Method to search the person arraylist for the person the user asked for
     //Method takes in the user provided name and list of names as parameters
